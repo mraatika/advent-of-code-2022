e19b2ea4d7a35dd38b683bd7650a1525f0537184
Miitri Raatikainen <raatikaine@gmail.com>
2022-12-12 18:03:28 +0200

    Puzzle 2.2

--- puzzle-2/src/main.rs
+++ puzzle-2/src/main.rs
@@ -1,6 +1,6 @@
 use ::std::fs;
 
-fn main() {
+fn puzzle2_1() {
     let input = fs::read_to_string("src/input.txt").expect("File read failed!");
     let sum = input
         .trim()
@@ -25,4 +25,44 @@ fn main() {
         });
 
     println!("Total: {sum}");
+}
+
+fn puzzle2_2() {
+    let input = fs::read_to_string("src/input.txt").expect("File read failed!");
+    let sum = input
+        .trim()
+        .lines()
+        .map(|rounds| rounds.split(" "))
+        .map(|mut hands| (hands.next().unwrap(), hands.next().unwrap()))
+        .fold(0, |sum, (them, result)| {
+            let points_from_result = match result {
+                "X" => 0,
+                "Y" => 3,
+                "Z" => 6,
+                _ => 0,
+            };
+
+            let my_hand = match them {
+                "A" if result == "X" => "C",
+                "B" if result == "X" => "A",
+                "C" if result == "X" => "B",
+
+                "A" if result == "Z" => "B",
+                "B" if result == "Z" => "C",
+                "C" if result == "Z" => "A",
+
+                _ => them,
+            };
+
+            let points_from_hand = my_hand.as_bytes()[0] as u32 - '@' as u32;
+
+            sum + points_from_hand + points_from_result
+        });
+
+    println!("Total: {sum}");
+}
+
+fn main() {
+    puzzle2_1();
+    puzzle2_2();
 }
